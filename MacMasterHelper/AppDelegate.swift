555f444feb5eeea05a8c49f63eb1f422f9d812bf
Artem Belkov <a.belkov@corp.mail.ru>
2019-07-09 17:39:21 +0300

    Initial Commit

---
+++ MacMasterHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacMasterHelper
+//
+//  Created by a.belkov on 09/07/2019.
+//  Copyright © 2019 Artem Belkov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacMasterHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacMasterHelper
+//
+//  Created by a.belkov on 09/07/2019.
+//  Copyright © 2019 Artem Belkov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
